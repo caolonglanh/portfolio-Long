eea23eddc2d03d95ee552198c6db2d706f3ebfcb
caolonglanh <longvo3544549@gmail.com>
2022-10-31 09:36:49 +0700

    update

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,18 +1,27 @@
 import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
+import Work2 from "../../assets/work3.jpg";
 
 export const projectsData = [
   {
     id: 1,
     image: Work1,
-    title: "Web Ecommerce",
+    title: "Web Ecommerce FE",
     category: "web",
+    src: "https://github.com/dangcongtuyen55/FE",
+  },
+  {
+    id: 3,
+    image: Work1,
+    title: "Web Ecommerce BE",
+    category: "web",
+    src: "https://github.com/caolonglanh/admin_ecommerce",
   },
   {
     id: 2,
     image: Work2,
     title: "App movie",
     category: "app",
+    src: "https://movie-web-9a8de.web.app/",
   },
 ];
 
